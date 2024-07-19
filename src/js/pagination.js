3106c3fd860678e5f576919fbbe7b2de2b9995f5
Kyrylo263 <kyryloz2000@gmail.com>
2024-07-19 10:52:12 +0300

    search-stroke-btn

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,6 +3,7 @@ const containerOfEvents = document.querySelector("#container-events-js")
 const btnLoadMore = document.querySelector("#load-more-js")
 const container = document.querySelector("#choose-country-select")
 const searchEvent = document.querySelector("#search-event-js")
+const searchBtn = document.querySelector(".search-btn")
 
 const BASE_ID = "https://app.ticketmaster.com/discovery/v2/events.json"
 
@@ -12,7 +13,7 @@ let preValue = valueCountry
 function getValue() {
     valueCountry = container.value
     if (preValue === valueCountry) {
-        sizeNumb+=1
+        sizeNumb += 1
     } else {
         sizeNumb = 0
         containerOfEvents.innerHTML = " "
@@ -23,12 +24,12 @@ function loadMore() {
 
     let searchValue = searchEvent.value
     fetch(`${BASE_ID}?apikey=G9QyoGR1xwN3XvcF7b5mopd7Xn36MiG5&page=${sizeNumb}&size=20&countryCode=${valueCountry}&keyword=${searchValue}`)
-    .then(response => response.json())
-    .then(events => {
-        console.log(events);
-        // console.log(`${BASE_ID}?apikey=G9QyoGR1xwN3XvcF7b5mopd7Xn36MiG5&page=${sizeNumb}&size=20&countryCode=${valueCountry}&keyword=Pink`);
+        .then(response => response.json())
+        .then(events => {
+            console.log(events);
+            // console.log(`${BASE_ID}?apikey=G9QyoGR1xwN3XvcF7b5mopd7Xn36MiG5&page=${sizeNumb}&size=20&countryCode=${valueCountry}&keyword=Pink`);
             for (const event of events._embedded.events) {
-                    
+
                 containerOfEvents.insertAdjacentHTML("beforeend", `
                     <div class="card">
                     <img src="" alt="">
@@ -37,17 +38,21 @@ function loadMore() {
                     <p class="place" id="place-js">${event._embedded.venues[0].name}</p>
                     </div>
                     `)
-            }            
-    })
-    .catch(error => alert("Sorry, there are no events in this country("))
+            }
+        })
+        .catch(error => alert("Sorry, there are no events in this country("))
 
 }
-        
+
+function searchContent() {
+    
+}
+
 container.addEventListener("click", getValue)
 btnLoadMore.addEventListener("click", loadMore)
-        
-        
-        
+// searchBtn.addEventListener("click", searchContent)
+
+
 
 
 
