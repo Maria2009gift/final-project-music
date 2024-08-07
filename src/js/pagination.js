078b62a40e543b143fb63836040f99401ca69eb2
Maria2009gift <kovalenkom2009@gmail.com>
2024-08-07 19:24:33 +0300

    fix problems with modal

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,3 @@
-import debounce from 'debounce';
 
 const containerOfEvents = document.querySelector("#container-events-js")
 const btnLoadMore = document.querySelector("#load-more-js")
@@ -7,6 +6,27 @@ const searchEvent = document.querySelector("#search-event-js")
 const searchBtn = document.querySelector(".search-btn")
 
 const BASE_ID = "https://app.ticketmaster.com/discovery/v2/events.json"
+
+function firstLoad() {
+    fetch(`${BASE_ID}?apikey=G9QyoGR1xwN3XvcF7b5mopd7Xn36MiG5&page=0&size=20`)
+    .then(response => response.json())
+    .then(events => {
+        console.log(events)
+        for (const event of events._embedded.events) {
+
+            containerOfEvents.insertAdjacentHTML("beforeend", `
+                <div class="card">
+                    <img class="img-card" id="${event.id}" src="${event.images[0].url}" alt="">
+                    <h5 class="title-event" id="title-event-js">${event.name}</h5>
+                    <p class="date" id="date-js">${event.dates.start.localDate}</p>
+                    <p class="place" id="place-js">${event._embedded.venues[0].name}</p>
+                </div>
+                `)
+        }
+    })
+}
+
+firstLoad()
 
 let sizeNumb = -1
 let valueCountry = " "
